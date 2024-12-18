4f60d249e5c20fa3f15158308c6b1b457a646407
Philip Emdokolo <phillip.emdokolo@student.moringaschool.com>
2024-12-18 10:04:25 +0300

    importing sqlalchemy requirements

---
+++ lib/models.py
@@ -0,0 +1,4 @@
+from sqlalchemy import Column, Interger, String, Float
+from sqlalchemy.text.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
