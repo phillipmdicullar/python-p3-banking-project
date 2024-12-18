b65700039df885292a873256b421db13bbce15c4
Philip Emdokolo <phillip.emdokolo@student.moringaschool.com>
2024-12-18 10:12:56 +0300

    created the deposit_account function

--- lib/helpers.py
+++ lib/helpers.py
@@ -5,3 +5,11 @@ def create_account(account_number, account_holder_name, initial_deposit=0):
     session.add(account)
     session.commit()
     print(f"Account created successfully for {account_holder_name} with account number {account_number}.")
+def deposit_money(account_number, amount):
+    account = session.query(Account).filter(Account.account_number == account_number).first()
+    if account:
+        account.balance += amount
+        session.commit()
+        print(f"{amount} deposited successfully to account number {account_number}.")
+    else:
+        print("Account not found.")
